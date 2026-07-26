f5af73e690d5e58b0c38f79096c5df399bec3657
Mars <mdlepana@gmail.com>
2026-07-26 23:20:33 +0300

    Resumit Week 3 Day 5

--- Week_3/Day_5/Exercise1_RandomSentence.py
+++ Week_3/Day_5/Exercise1_RandomSentence.py
@@ -1,6 +1,7 @@
 # 🌟 Exercise 1 — Random Sentence Generator
 import random
 from pathlib import Path
+import json
 
 def get_words_from_file(file_path):
     file_path = Path(__file__).parent / "words.txt"
@@ -29,3 +30,33 @@ def main():
     print(f"Random sentence: {sentence}")   
 
 main()
+
+#Exercise 2 — JSON Manipulation
+
+sampleJson = """{
+   "company":{
+      "employee":{
+         "name":"emma",
+         "payable":{
+            "salary":7000,
+            "bonus":800
+         }
+      }
+   }
+}"""
+
+
+#Parse the JSON string using json.loads()
+data = json.loads(sampleJson) #load string to dictionary
+
+print(f"Salary: {data['company']['employee']['payable']['salary']}")
+data['company']['employee']['birthdate'] = "1990-01-01" #add new key-value pair
+
+with open("employee.json","w") as f:
+    json.dump(data, f, indent=2) #dump dictionary to file with indentation
+    print("Modified data saved to employee.json")
+
+with open("employee.json", "r") as f:
+    verified = json.load(f) #load file to dictionary 
+
+print(f"Verified birthdate: {verified['company']['employee']['birthdate']}")
---
+++ Week_4/Day_1/DailyChallenge.py
@@ -0,0 +1,23 @@
+# Instructions
+# Part 1 : Quizz :
+# Answer the following questions
+
+# What is a class?
+# What is an instance?
+# What is encapsulation?
+# What is abstraction?
+# What is inheritance?
+# What is multiple inheritance?
+# What is polymorphism?
+# What is method resolution order or MRO?
+
+
+# Part 2: Create a deck of cards class.
+# The Deck of cards class should NOT inherit from a Card class.
+
+# The requirements are as follows:
+
+# The Card class should have a suit (Hearts, Diamonds, Clubs, Spades) and a value (A,2,3,4,5,6,7,8,9,10,J,Q,K)
+# The Deck class :
+# should have a shuffle method which makes sure the deck of cards has all 52 cards and then rearranges them randomly.
+# should have a method called deal which deals a single card from the deck. After a card is dealt, it should be removed from the deck.
